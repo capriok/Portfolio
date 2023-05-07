35225312e0d8cb82f4ee946962a96fd876292b8b
Kyle Caprio <capriok7@gmail.com>
2023-05-06 17:30:15 -0700

    work meta

--- app/(app)/work/page.tsx
+++ app/(app)/work/page.tsx
@@ -2,6 +2,10 @@ import { getProjects } from "@api/getProjects"
 import ProjectList from "@components/ProjectList"
 import Contact from "@components/Contact"
 
+export const metadata = {
+  title: "Kyle Caprio | Work",
+  description: "A function a day keeps the doctor away.",
+}
 export default async function Work() {
   const projects = getProjects(true)
 
