c07c6e3f1ca76426c66e43151f7c27b2a6148e35
Kyle Caprio <capriok7@gmail.com>
2025-12-30 17:20:11 -0700

    add codings

--- src/app/(app)/[project]/page.tsx
+++ src/app/(app)/[project]/page.tsx
@@ -8,8 +8,9 @@ export async function generateStaticParams() {
   }))
 }
 
-export async function generateMetadata({ params }) {
-  const project = projects.find((project) => project.slug === params.project)
+export async function generateMetadata({ params }: { params: Promise<{ project: string }> }) {
+  const resolvedParams = await params
+  const project = projects.find((project) => project.slug === resolvedParams.project)
 
   return {
     title: "Kyle Caprio | Project",
@@ -20,15 +21,18 @@ export async function generateMetadata({ params }) {
   }
 }
 
-
-export default async function ProjectTemplate({ params }: { params: Promise<{ project: string }> }) {
-  const { project: projectSlug } = await params;
-  const projects = getProjects(true);
-  const project = projects.find((p) => p.slug === projectSlug)!;
+export default async function ProjectTemplate({
+  params,
+}: {
+  params: Promise<{ project: string }>
+}) {
+  const resolvedParams = await params
+  const { project: projectSlug } = resolvedParams
+  const projects = getProjects(true)
+  const project = projects.find((p) => p.slug === projectSlug)!
   return (
     <>
       <ProjectView project={project} />
     </>
-  );
+  )
 }
-
